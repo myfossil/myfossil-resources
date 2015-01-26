ce7b26e744eba101e1662eb3291fccd5c25c0d42
Brandon Wood <btwood@geometeor.com>
2015-01-26 17:15:53 -0500

    BUG: Disabled Google Map for Events temporarily

--- public/partials/events.php
+++ public/partials/events.php
@@ -1,7 +1,7 @@
 <?php
 namespace myFOSSIL\Plugin\Resources;
 
-require_once 'map.php';
+// require_once 'map.php';
 require_once 'events/filters.php';
 require_once 'events/list.php';
 require_once 'events/event-template.php';
